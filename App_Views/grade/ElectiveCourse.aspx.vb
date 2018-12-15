0dc445b0bd235a835b6d924a1a0e9e24a7b3f86b
cartoonYu <13414156340@163.com>
2018-12-15 23:57:49 +0800

    update

---
+++ App_Views/grade/ElectiveCourse.aspx.vb
@@ -0,0 +1,5 @@
+﻿
+Partial Class App_Views_grade_ElectiveCourse
+    Inherits System.Web.UI.Page
+
+End Class
--- App_Views/report/GradeReport2.aspx.cs
+++ App_Views/report/GradeReport2.aspx.cs
@@ -11,4 +11,29 @@ public partial class App_Views_report_GradeReport2 : System.Web.UI.Page
     {
 
     }
+    protected void bt_f_Click(object sender, ImageClickEventArgs e)
+    {
+        Response.Clear();
+        Response.BufferOutput = true;
+        //设定输出的字符集
+        Response.Charset = "GB2312";
+        //假定导出的文件名为FileName.doc
+        Response.AppendHeader("Content-Disposition", "attachment;filename=FileName.doc");
+        Response.ContentEncoding = System.Text.Encoding.GetEncoding("GB2312");
+        ////设置导出文件的格式
+        Response.ContentType = "application/ms-word";
+        //关闭ViewState
+        GridView1.EnableViewState = false;
+        System.Globalization.CultureInfo cultureInfo = new System.Globalization.CultureInfo("ZH-CN", true);
+        System.IO.StringWriter stringWriter = new System.IO.StringWriter(cultureInfo);
+        System.Web.UI.HtmlTextWriter textWriter = new System.Web.UI.HtmlTextWriter(stringWriter);
+        GridView1.RenderControl(textWriter);
+        ////把HTML写回浏览器
+        Response.Write(stringWriter.ToString());
+        Response.End();
+    }
+    public override void VerifyRenderingInServerForm(Control control)
+    {
+        //base.VerifyRenderingInServerForm(control);
+    }
 }
